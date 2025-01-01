5f1aa10b73c20612cdf8ca652ba618eb5a8d96f3
Abdulhefiz Abdurahman <105729069+Abdl-Hafeez@users.noreply.github.com>
2025-01-02 00:53:14 +0300

    Modify the code for the default grid and add transition effect for the colors

--- script.js
+++ script.js
@@ -2,18 +2,6 @@ const input = document.querySelector('#num_of_squares');
 const inputBtn = document.querySelector('#input-btn');
 const container = document.querySelector('.container');
 
-for(let i = 1; i <= 16; i++) {
-    for(let j = 1; j <= 16; j++) {
-        const divs = document.createElement('div');
-        divs.setAttribute('style', 
-                'flex: 0 0 25px;  border: 1px solid  #333;');           
-        divs.addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
-        });
-        container.appendChild(divs);
-    } 
-}
-
 function renderGrid(input) {
     const squareSize = 400 / input;
     container.innerHTML = '';
@@ -23,7 +11,8 @@ function renderGrid(input) {
             divs.setAttribute('style', 
                 `flex: 0 0 ${squareSize}px; 
                 height: ${squareSize}px; 
-                border: 1px solid  #333;`);         
+                border: 1px solid  #333;
+                transition: background-color 0.3s ease;`);         
             divs.addEventListener('mouseover', (event) => {
                 event.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
             });
@@ -31,6 +20,10 @@ function renderGrid(input) {
         } 
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderGrid(16);
+});
 
 inputBtn.addEventListener('click',() => {
     const userInput = parseInt(input.value);
